2399b86eb64e81dcb31c9d5131e178283f27b048
Hamed Nejat <59973245+HNXJ@users.noreply.github.com>
2021-04-25 02:40:00 +0430

    Graph animated, imageio

--- Animator.py
+++ Animator.py
@@ -1,12 +1,36 @@
 from images2gif import writeGif
 from PIL import Image
+import imageio
 import os
 
 
-def make_gif(folderpath=None, filename=None, ftype=".png", duration=1):
+# def make_gif(folderpath=None, filename=None, ftype=".png", duration=1):
     
-    file_names = sorted((fn for fn in os.listdir('.') if fn.endswith(ftype)))
-    images = [Image.open(fn) for fn in file_names]
-    writeGif(filename, images, duration=duration)
-    return
+#     file_names = sorted((fn for fn in os.listdir(folderpath) if fn.endswith(ftype)))
+#     for i in range(len(file_names)):
+#         file_names[i] = folderpath + "/" + file_names[i]
+        
+#     # try:
+#         images = [Image.open(fn) for fn in file_names]
+#          writeGif(filename, images, duration=duration)
+#     # except:
+#         print("Cannot make gif, check if folderpath is empty, address is valid or "
+#            + "library is installed properly")
+#     return
 
+
+def make_gif(path=None, fname="t.gif", duration=1):
+    
+    image_folder = os.fsencode(path)
+    filenames = []
+    
+    for file in os.listdir(image_folder):
+        filename = os.fsdecode(file)
+        if filename.endswith( ('.jpeg', '.png', '.gif') ):
+            filenames.append(path + filename)
+    
+    # filenames.sort()
+    images = list(map(lambda filename: imageio.imread(filename), filenames))
+    imageio.mimsave(os.path.join(fname), images, duration=duration) 
+    
+    return
