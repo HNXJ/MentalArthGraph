135502ab551fdedfa37e82b7004d3a43c2574764
Hamed Nejat <59973245+HNXJ@users.noreply.github.com>
2021-04-26 05:59:48 +0430

    V4.4

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ frame = 90
 # ds, ds_pearson, ds_spectral = datasets_preparation(frames=frame, order=4, cf1=15,
                                                     # cf2=25, mth="coherence")
 # ds_p0, ds_p1 = split_count_quality(ds_pearson, id1="0", id2="1")
-# ds_s0, ds_s1 = split_count_quality(ds_spectral, id1="0", id2="1")
+ds_s0, ds_s1 = split_count_quality(ds_spectral, id1="0", id2="1")
 
 # # # Data loading and preprocessing: mutual information
 # # chs0 = chs1
@@ -51,6 +51,7 @@ frame = 90
 # save_list(ds_f1, "Data/f1_" + str(frame) + "f_[15-25].txt")
 
 # # Load later:
+# ds_spectral = load_list("Data/spectral_90f_[15-25].txt")
 # ds_f0 = load_list("Data/f0_1f_[15-25].txt")
 # ds_f1 = load_list("Data/f1_1f_[15-25].txt")
 # ds_s0 = load_list("Data/s0_1f_[15-25].txt")
@@ -97,7 +98,7 @@ frame = 90
 # k = tsne_cluster(X=x, Y=y, components=3, visualize=True, iterations=10000, tit="TSNE-3")
 # in_shape = [1600]
 
-
+ 
 
 
 # # Deep classification parts (colab recommended for this part)
@@ -118,10 +119,10 @@ frame = 90
 # run_graph_visualize(ds_pearson, mode="Spectral", split="count-quality")
 
 
-# # Animator
+# Animator
 # make_gif(path="Graphs/Spectral_Good_count-quality_90_f_bandpass[15_25]Hz/",
-#           fname="Animate/sg90_15_25.gif", duration=0.23)
+#           fname="Animate/sg90_15_25.gif", duration=0.23, f=70)
 # make_gif(path="Graphs/Spectral_Bad_count-quality_90_f_bandpass[15_25]Hz/",
-#           fname="Animate/sb90_15_25.gif", duration=0.23)
+#           fname="Animate/sb90_15_25.gif", duration=0.23, f=70)
 # make_gif(path="Graphs/Spectral_Good_count-quality_10_f_bandpass[32_38]Hz/",
 #           fname="Animate/sg10_32_38.gif", duration=0.12)
