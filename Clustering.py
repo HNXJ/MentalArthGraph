dd719f0bd9c6bdafb66b9e6ea05b8c6afbcfb4e9
Hamed Nejat <59973245+HNXJ@users.noreply.github.com>
2021-04-28 03:29:57 +0430

    V4.6

--- Clustering.py
+++ Clustering.py
@@ -1,40 +1,45 @@
+import os
 import numpy as np
 from sklearn.manifold import TSNE
+
 from matplotlib import pyplot as plt
 from sklearn.decomposition import PCA
 
-def pca_cluster(X=None, Y=None, components=2, visualize=True, tit=None):
+
+def pca_cluster(X=None, Y=None, components=2, visualize=True, tit=None, save=False, name=""):
     
     pca = PCA(n_components=components)
     x = pca.fit_transform(X)
     x = x - np.min(np.min(x))
     x = x / np.max(np.max(x))
+    
     if visualize:
-        tsne_plot(x, Y)
+        cluster_plot(x, Y, tit, save, name)
         
     return x
 
 
-def tsne_cluster(X=None, Y=None, components=2, visualize=True, iterations=100, tit=None):
+def tsne_cluster(X=None, Y=None, components=2, visualize=True, iterations=100, tit=None, save=False, name=""):
     
     tsne = TSNE(n_components=components, n_iter=iterations, perplexity=3,
                 learning_rate=350, init='random')
     x = tsne.fit_transform(X, Y)
     x = x - np.min(np.min(x))
     x = x / np.max(np.max(x))
+    
     if visualize:
-        tsne_plot(x, Y)
+        cluster_plot(x, Y, tit, save, name)
         
     return x
 
 
-def tsne_plot(X=None, Y=None, tit=None):
+def cluster_plot(X=None, Y=None, tit=None, save=False, name=""):
     
     if X.shape[1] == 2:        
-        tsne_2d_plot(X, Y)
+        scatter_2d_plot(X, Y, tit, save, name)
     
     elif X.shape[1] == 3:    
-        tsne_3d_plot(X, Y)
+        scatter_3d_plot(X, Y, tit, save, name)
     
     else:            
         print("Cannot plot with " + str(X.shape[1]) + " dimension")
@@ -42,27 +47,43 @@ def tsne_plot(X=None, Y=None, tit=None):
     return
 
 
-def tsne_2d_plot(X=None, Y=None, tit=None):
+def scatter_2d_plot(X=None, Y=None, tit=None, save=False, name=""):
     
     fig, ax = plt.subplots(figsize=(30, 20))
     for i in range(X.shape[0]):
+        ax.scatter(X[i, 0], X[i, 1], color=[Y[i, 0], 0.4, Y[i, 1]], marker='o', linewidth=15 - Y[i, 1]*7)
+        if Y[i, 1] == 1:
+            ax.text(X[i, 0], X[i, 1] + X[i, 1]*0.02, str(int(i/(X.shape[0]/20))))
+        else:
+            ax.text(X[i, 0], X[i, 1] - X[i, 1]*0.02, str(int(i/(X.shape[0]/20))))
         
-        ax.scatter(X[i, 0], X[i, 1], color=[Y[i, 0], 0.4, Y[i, 1]], marker='o', linewidth=15)
-    
     ax.grid(True)
     ax.set_title(tit)
     fig.show()
+    if save:
+        try:
+            fig.savefig("Plots/" + name + ".png")
+        except:
+            os.mkdir("Plots")
+            fig.savefig("Plots/" + name + ".png")
     return
 
 
-def tsne_3d_plot(X=None, Y=None, tit=None):
+def scatter_3d_plot(X=None, Y=None, tit=None, save=False, name=""):
+    
     fig = plt.figure(figsize=(30, 20))
     ax = fig.add_subplot(projection='3d')
     for i in range(X.shape[0]):
         
-        ax.scatter(X[i, 0], X[i, 1], X[i, 2], color=[Y[i, 0], 0.4, Y[i, 1]], marker='o', linewidth=12)
+        ax.scatter(X[i, 0], X[i, 1], X[i, 2], color=[Y[i, 0], 0.4, Y[i, 1]], marker='o', linewidth=12 - Y[i, 1]*6)
     
     ax.set_title(tit)
     fig.show()
+    if save:
+        try:
+            fig.savefig("Plots/" + name + ".png")
+        except:
+            os.mkdir("Plots")
+            fig.savefig("Plots/" + name + ".png")
     return
 
